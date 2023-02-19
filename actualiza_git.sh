7665d80c590a363f89076d85a6d7a7e8182b058d
Daniel Bitria Ferrero <bitriaferrero@gmail.com>
2023-02-19 00:55:27 +0000

    Script sin funcionar

--- actualiza_git.sh
+++ actualiza_git.sh
@@ -1,2 +1,20 @@
+# SCRIPT SUBIDA A GIT Daniel.Bitria.Ferrero
+echo 'Nombre de fichero o directorio:'
+read fichero
+echo 'Descripcion:'
+read descripcion
+
+#STATUS
+echo 'ficheros actualizados....'
 git status
 
+#Agregar fichero ansible a git
+git add .
+git commit  -m  $descripcion
+git push origin main
+
+#git status
+#git add .
+#git status
+#git commit -m "Added install/remove playbooks fo apache"
+#git push origin main
